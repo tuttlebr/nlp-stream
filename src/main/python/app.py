364a0d4fe7fc86e7ea04e0e54c9c42ae7f568d9b
Brandon Tuttle <tuttlebr@ncc-1701.biz>
2021-11-10 22:52:41 -0500

    switched to twitter v2 api

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,21 +1,48 @@
-from TwitterAPI import TwitterAPI
+from searchtweets import (
+    gen_request_parameters,
+    load_credentials,
+    collect_results,
+)
 from os import getenv
 from logging import info, basicConfig
 from datetime import datetime
 from inference_management import *
-import numpy as np
 import re
 
 
-CONSUMER_KEY = getenv("CONSUMER_KEY")
-CONSUMER_SECRET = getenv("CONSUMER_SECRET")
-ACCESS_TOKEN_KEY = getenv("ACCESS_TOKEN_KEY")
-ACCESS_TOKEN_SECRET = getenv("ACCESS_TOKEN_SECRET")
 TRACK_TERM = getenv("TRACK_TERM")
 TIMESTAMP = datetime.now()
 LOG_ID = TIMESTAMP.strftime("%Y%m%d_%H%M%S")
 COMPREHENSION_ENGINE = ZeroComprehensionEngine()
 TRACK_ENTAILMENTS = getenv("TRACK_ENTAILMENTS")
+
+
+def clean_tweet(tweet):
+    temp = tweet.lower()
+    temp = re.sub("'", "", temp)
+    temp = re.sub("@[A-Za-z0-9_]+", "", temp)
+    temp = re.sub("#[A-Za-z0-9_]+", "", temp)
+    temp = re.sub(r"http\S+", "", temp)
+    temp = re.sub("[()!?]", " ", temp)
+    temp = re.sub("\[.*?\]", " ", temp)
+    temp = re.sub("[^a-z0-9]", " ", temp)
+    temp = temp.split()
+    temp = " ".join(word for word in temp)
+    return temp.replace("\n", "")
+
+
+search_args = load_credentials(
+    filename="/app/.twitter_keys.yaml",
+    yaml_key="search_tweets_v2",
+    env_overwrite=False,
+)
+
+query = gen_request_parameters(
+    TRACK_TERM, results_per_call=100, granularity=None
+)
+tweets = collect_results(
+    query, max_tweets=5000, result_stream_args=search_args
+)
 
 basicConfig(
     filename="/logs/{}.log".format(LOG_ID),
@@ -23,34 +50,16 @@ basicConfig(
     format="%(asctime)s, %(message)s",
     datefmt="%H:%M:%S",
 )
-
-
-def clean_tweet(tweet):
-    if type(tweet) == np.float:
-        return ""
-    temp = tweet.lower()
-    temp = re.sub("'", "", temp)
-    temp = re.sub("@[A-Za-z0-9_]+","", temp)
-    temp = re.sub("#[A-Za-z0-9_]+","", temp)
-    temp = re.sub(r'http\S+', '', temp)
-    temp = re.sub('[()!?]', ' ', temp)
-    temp = re.sub('\[.*?\]',' ', temp)
-    temp = re.sub("[^a-z0-9]"," ", temp)
-    temp = temp.split()
-    temp = " ".join(word for word in temp)
-    return temp.replace("\n", "")
-
-api = TwitterAPI(
-    CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET
-)
-
-r = api.request("statuses/filter", {"track": TRACK_TERM})
 info("comment,{},sentiment".format(TRACK_ENTAILMENTS))
-for item in r:
-    message = item["text"] if "text" in item else item
-    message = clean_tweet(message)
-    message = COMPREHENSION_ENGINE.free_comprehend(
-        message, TRACK_ENTAILMENTS.split(",")
-    )
 
-    info(message)
+
+for tweet in tweets:
+    for item in tweet["data"]:
+        message = item["text"] if "text" in item else item
+        message = clean_tweet(message)
+        if len(message) > 10:
+            message = COMPREHENSION_ENGINE.free_comprehend(
+                message, TRACK_ENTAILMENTS.split(",")
+            )
+
+            info(message)
